579625a2bfed2a21c3a5062a41d1167ceda72309
Mohammad Ataei <mammadataei@gmail.com>
2022-07-25 16:31:09 +0430

    refactor: update `toBeRequired` using`dom-assertions`

--- src/to-be-required.js
+++ src/to-be-required.js
@@ -1,58 +1,7 @@
-import {checkHtmlElement, getTag} from './utils'
-
-// form elements that support 'required'
-const FORM_TAGS = ['select', 'textarea']
-
-const ARIA_FORM_TAGS = ['input', 'select', 'textarea']
-
-const UNSUPPORTED_INPUT_TYPES = [
-  'color',
-  'hidden',
-  'range',
-  'submit',
-  'image',
-  'reset',
-]
-
-const SUPPORTED_ARIA_ROLES = [
-  'combobox',
-  'gridcell',
-  'radiogroup',
-  'spinbutton',
-  'tree',
-]
-
-function isRequiredOnFormTagsExceptInput(element) {
-  return FORM_TAGS.includes(getTag(element)) && element.hasAttribute('required')
-}
-
-function isRequiredOnSupportedInput(element) {
-  return (
-    getTag(element) === 'input' &&
-    element.hasAttribute('required') &&
-    ((element.hasAttribute('type') &&
-      !UNSUPPORTED_INPUT_TYPES.includes(element.getAttribute('type'))) ||
-      !element.hasAttribute('type'))
-  )
-}
-
-function isElementRequiredByARIA(element) {
-  return (
-    element.hasAttribute('aria-required') &&
-    element.getAttribute('aria-required') === 'true' &&
-    (ARIA_FORM_TAGS.includes(getTag(element)) ||
-      (element.hasAttribute('role') &&
-        SUPPORTED_ARIA_ROLES.includes(element.getAttribute('role'))))
-  )
-}
+import {assertElementIsRequired} from 'dom-assertions'
 
 export function toBeRequired(element) {
-  checkHtmlElement(element, toBeRequired, this)
-
-  const isRequired =
-    isRequiredOnFormTagsExceptInput(element) ||
-    isRequiredOnSupportedInput(element) ||
-    isElementRequiredByARIA(element)
+  const isRequired = assertElementIsRequired(element).pass
 
   return {
     pass: isRequired,
